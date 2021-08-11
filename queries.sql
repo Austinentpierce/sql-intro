05aad4f83d8e08019c816d5c0207df18269083c6
Austin Pierce <Austinentpierce@gmail.com>
2021-08-11 19:20:09 -0400

    inserting new order in order table

--- queries.sql
+++ queries.sql
@@ -22,7 +22,7 @@ VALUES ('Gary Minnis', '444', 'Butcher', '42000', false);
 INSERT INTO "Employees" ("FullName", "Salary", "JobPosition", "PhoneExtension", "IsPartTime")
 VALUES ('Ivan Ruiz', '235', 'Cook', '33624', false);
 
-SERT INTO "Employees" ("FullName", "Salary", "JobPosition", "PhoneExtension", "IsPartTime")
+INSERT INTO "Employees" ("FullName", "Salary", "JobPosition", "PhoneExtension", "IsPartTime")
 VALUES ('Ruby Cherry', '660', 'Manager', '66600', false);
 
 INSERT INTO "Employees" ("FullName", "Salary", "JobPosition", "PhoneExtension", "IsPartTime")
@@ -42,3 +42,51 @@ WHERE "JobPosition" = 'Cook';
 DELETE FROM "Employees" WHERE "FullName" = 'Lazy Larry'; 
 
 ALTER TABLE "Employees" ADD COLUMN "ParkingSpot" VARCHAR(10);
+
+---------WEDNESDAY-----------------------HOMEWORK-------
+CREATE TABLE "Departments" (
+"Id" SERIAL PRIMARY KEY,
+"DepartmentName" TEXT,
+"Building" TEXT, 
+);
+
+ALTER TABLE "Employees" ADD COLUMN  "DepartmentId" INTEGER NULL REFERENCES "Departments" ("Id");
+
+CREATE TABLE "Products" (
+"Id" SERIAL PRIMARY KEY,
+"Price" DECIMAL,
+"Name" TEXT,
+"Description" TEXT,
+"QuantityInStock" INT
+);
+
+CREATE TABLE "Orders" (
+"Id" SERIAL PRIMARY KEY,
+"OrderNumber" TEXT,
+"DatePlaced" DATE,
+"Email" TEXT
+);
+
+
+
+INSERT INTO "Departments" ("DepartmentName", "Building")
+VALUES ('Development', 'Main');
+
+INSERT INTO "Departments" ("DepartmentName", "Building")
+VALUES ('Marketing', 'North');
+
+INSERT INTO "Employees" ("FullName", "Salary", "JobPosition", "PhoneExtension", "IsPartTime", "DepartmentId")
+VALUES ('Tim Smith', '40000', 'Programmer', '123', false, 1);
+
+INSERT INTO "Employees" ("FullName", "Salary", "JobPosition", "PhoneExtension", "IsPartTime", "DepartmentId")
+VALUES ('Barbara Ramsey', '80000', 'Manager', '234', false, 1);
+
+
+INSERT INTO "Employees" ("FullName", "Salary", "JobPosition", "PhoneExtension", "IsPartTime", "DepartmentId")
+VALUES ('Tom Jones', '32000', 'Admin', '456', true, 2); 
+
+INSERT INTO "Products" ("Price", "Name", "Description", "QuantityInStock")
+VALUES (12.45, 'Widget', 'The Original Widget', 100);
+
+INSERT INTO "Products" ("Price", "Name", "Description", "QuantityInStock")
+VALUES (99.99, 'Flowbee', 'Perfect for haircuts', 3);
